77e2903193d8d13691ff0705dff4ffd960d742a0
ed55567 <emmanueldaniels37@gmail.com>
2021-07-23 02:24:23 -0400

    added dino api using asyncand fetch also made get route

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+});
+
+app.get('/dinoname', async(request, response) =>{
+  
+  const fetchApi = await fetch("https://alexnormand-dino-ipsum.p.rapidapi.com/?paragraphs=1&words=2&format=json", {
+	"method": "GET",
+	"headers": {
+	
+	}
+});
+ const dinoNameReposnse = await fetchApi.json();
+ console.log(dinoNameReposnse);
+ response.json(dinoNameReposnse);
 });
---
+++ public/script.js
@@ -0,0 +1,14 @@
+console.log('script.js loaded');
+
+document.querySelector('#btnLoad').addEventListener('click', () =>{
+  getDinoName();
+});
+
+
+
+async function getDinoName(){
+  const response = await fetch('/dinoname');
+  const data = await response.json();
+  let dinoName = data[0].join(' ');
+  console.log(dinoName);
+}
